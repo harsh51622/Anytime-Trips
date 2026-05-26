c73eb53304cfdc232a72acf51134366a56ea7714
harsh <hkumar51622@gmail.com>
2026-05-26 15:08:33 +0530

    Fix database config

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://anytime-trips.onrender.com/api/",
 });
 
 // auto attach token
@@ -17,4 +17,3 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
-
