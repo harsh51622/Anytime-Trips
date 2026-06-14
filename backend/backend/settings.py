47b8ba3597f9cee48282392c2fab2391fca2d748
Harsh kumar <hkumar51622@gmail.com>
2026-06-14 09:54:19 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -104,10 +104,11 @@ USE_TZ = True
 # MEDIA FILES
 
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
